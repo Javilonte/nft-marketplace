5eea0d13202cac6b1e9f9b65963751e549a8dae1
Beicker Zambrano <beicker998@gmail.com>
2021-11-15 16:42:46 -0500

    Comment previous layout code

--- pages/_app.js
+++ pages/_app.js
@@ -17,23 +17,26 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <div>
+        {/*
+           *
         <nav className="border-b p-6">
-          <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
           <div className="flex mt-4">
             <Link href="/">
-              <a className="mr-4 text-pink-500">Home</a>
+            <a className="mr-4 text-pink-500">Home</a>
             </Link>
             <Link href="/create-item">
-              <a className="mr-6 text-pink-500">Sell Digital Asset</a>
+            <a className="mr-6 text-pink-500">Sell Digital Asset</a>
             </Link>
             <Link href="/my-assets">
-              <a className="mr-6 text-pink-500">My Digital Assets</a>
+            <a className="mr-6 text-pink-500">My Digital Assets</a>
             </Link>
             <Link href="/creator-dashboard">
-              <a className="mr-6 text-pink-500">Creator Dashboard</a>
+            <a className="mr-6 text-pink-500">Creator Dashboard</a>
             </Link>
           </div>
         </nav>
+            * */}
         <Header />
         <Component {...pageProps} />
         <ScrollToTop />
