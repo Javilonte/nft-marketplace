41c81bd1e0a8bd473633cca52dcf64b9f162e0ba
Beicker Zambrano <beicker998@gmail.com>
2021-11-18 10:38:52 -0500

    Import global styles into create item page

--- pages/create-item.js
+++ pages/create-item.js
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import { ethers } from 'ethers'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
-import { useRouter } from 'next/router'
-import Web3Modal from 'web3modal'
+import { useState } from "react";
+import { ethers } from "ethers";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { useRouter } from "next/router";
+import Web3Modal from "web3modal";
+import Clock from "../components/Clock";
+import Footer from "../components/footer";
+import { createGlobalStyle } from "styled-components";
 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
-import {
-  nftaddress, nftmarketaddress
-} from '../config'
+import { nftaddress, nftmarketaddress } from "../config";
 
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
